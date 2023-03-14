424cb954921247ef9a0d944ecbc859d251d33993
GabiMonteiros <monteiro8gabi@gmail.com>
2023-03-14 19:48:52 +0100

    burger menu

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,14 @@
 import React, {useState} from 'react'
-import * as GrIcons from "react-icons/gr";
+import * as CgIcons from "react-icons/cg";
+import * as AiIcons from 'react-icons/ai';
 import {Link} from 'react-router-dom'
 import  SideBarData  from '../SideBarData';
 
 import './index.css'
+import { IconContext } from 'react-icons/lib';
+
+// import { IconContext } from 'react-icons CgMenuHotdog';
+
 
 
 
@@ -14,73 +19,67 @@ function Navbar() {
 
 
   return (
-    <>
-        <div className="navbar" data-scroll-section>
-            {/* navbar */}
-            <div className="nav-item"> 
-             {/* menu-bars */}
-                <Link
-                    to="/"
-                    className="nav-links"  
-                    onClick={showSidebar}    
-                >
-                    menu 
-                </Link> 
+        <>
+            <IconContext.Provider value={{color: '#e3be92'}}>
+                <div className="navbar" data-scroll-section>
+                    {/* navbar */}
+                        <Link
+                            to='/'
+                           className="menu-bars"     
+                        >
+                            <CgIcons.CgMenuHotdog onClick={showSidebar} />
+                        </Link> 
+                    
+                    <nav className={ sidebar ? 'nav-menu active' : 'nav-menu'}>
+                        <ul className= 'nav-menu-items' onClick={showSidebar}>
+                            <li className='navbar-toggle'>
+                                <Link  to="/" className='menu-bars close' >
 
-                
-            </div>
-            <nav className={ sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className= 'nav-menu-items'>
-                    <li className='navbar-toggle'>
-                        <Link  to="/" className='menu-bars' >
-
-                            <GrIcons.GrClose/>
-                        </Link>
-                    </li>
-                    {SideBarData.map((item, index) => {
-                        return(
-                            <li key={index} className={item.clName}>
-                                <Link to={item.path}>
-                                    <span>{item.title}</span>
+                                    <AiIcons.AiOutlineClose/>
                                 </Link>
                             </li>
-                        )
-                    })}
-                </ul>
+                            {SideBarData.map((item, index) => {
+                                return(
+                                    <li key={index} className={item.clName}>
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
 
-                    
-            </nav>
-
-            <div className="nav-item">
-                <Link
-                    to="/"
-                    className="nav-links"
-                                
-                >
-                    Decolonize the Image
-                </Link>    
-            </div>
-
-            <div className="nav-item">
                             
-                <Link
-                    to="/who-am-I"
-                    className="nav-links"
+                    </nav>
+
+                    <div className="nav-item">
+                        <Link
+                            to="/"
+                            className="nav-links"
+                                        
+                        >
+                            Decolonize the Image
+                        </Link>    
+                    </div>
+
+                    <div className="nav-item">
                                     
-                >
-                    Who Am I?
-                </Link>    
+                        <Link
+                            to="/who-am-I"
+                            className="nav-links"
+                                            
+                        >
+                            Who Am I?
+                        </Link>    
 
-            </div>
-
-            
-
-            
-        </div> 
-
-
-    </>
-  );
+                    </div>
+ 
+                </div> 
+                            
+            </IconContext.Provider>
+        </>
+    );
 }
 
 export default Navbar
--- src/components/SideBarData/index.jsx
+++ src/components/SideBarData/index.jsx
@@ -1,36 +1,39 @@
-//import React from 'react'
-//import * as GrIcons from "react-icons/gr";
+//  import React from 'react'
+//  import * as GrIcons from "react-icons/gr";
+// import * as CgIcons from "react-icons/cg";
+
 
 export const SidebarData = [
     {
         title: 'Home',
         path: '/',
+        
         clName: 'nav-text',
     },
     {
         title: 'Whitecentrism',
         path: '/white-centrism',
-        className: 'nav-text',
+        clName: 'nav-text',
     },
     {
         title: 'Violence with non-white Bodies',
         path: '/violence-with-non-white-bodies',
-        className: 'nav-text',
+        clName: 'nav-text',
     },
     {
         title: 'Sexism/Dehumanization',
         path: '/sexism',
-        className: 'nav-text',
+        clName: 'nav-text',
     },
     {
         title: 'Always Serving',
         path: '/non-white-serving',
-        className: 'nav-text',
+        clName: 'nav-text',
     },
     {
         title: 'Google Image Search',
         path: '/google-image-search',
-        className: 'nav-text',
+        clName:'nav-text',
     },
 ]
 
@@ -39,64 +42,3 @@ export default SidebarData
 
 
 
-/* <ul className= 'header-menu'>
-                    <li className="nav-item">
-                                
-                        <Link
-                            to="/white-centrism"
-                            className="nav-links"
-                                    
-                        >
-                            Whitecentrism
-                        </Link>
-                                
-                    </li>      
-                    <li className="nav-item">
-                                
-                        <Link
-                            to="/violence-with-non-white-bodies"
-                            className="nav-links"
-                                
-                        >
-                            Violence with non-white Bodies
-                        </Link>
-                            
-                    </li>  
-                    <li className="nav-item">
-                                
-                        <Link
-                            to="/sexism"
-                            className="nav-links"
-                            
-                        >
-                            Sexism/Dehumanization
-                        </Link>
-                            
-                    </li>   
-                        
-                    <li className="nav-item">
-                                    
-                        <Link
-                            to="/non-white-serving"
-                            className="nav-links"
-                                    
-                        >
-                            Always Serving
-                        </Link>
-                                
-                    </li>   
-
-                    <li className="nav-item">
-                                
-                        <Link
-                            to="/google-image-search"
-                            className="nav-links"
-                                
-                        >
-                            Google Image Search
-                        </Link>
-                            
-                    </li> 
-
-                        
-                </ul>  */
