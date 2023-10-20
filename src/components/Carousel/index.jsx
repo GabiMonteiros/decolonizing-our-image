5d982fe029d41f3c41d02cff96d642464d9134f9
GabiMonteiros <monteiro8gabi@gmail.com>
2023-10-20 18:53:06 +0200

    carousel functionality01

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,38 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 import * as AiIcons from 'react-icons/ai';
 import { IconContext } from 'react-icons';
+import { CarouselData } from './carouselData';
 
-function Carousel() {
-  return (
-    <>
-        <IconContext.Provider value={{color: '#e3be92'}} >
+const Carousel = ({carousels}) => {
+    const [current, setCurrent ] = useState(0);
+    const length = carousels.length;
 
-            <div className="wrapper">
-                <ul className="angle-left">
-                    <AiIcons.AiOutlineDoubleLeft/> 
-                </ul>
-        
-                <div className="carousel">
-                    <img src="images/01JeanBaptisteDebret1823.png" alt="Debret Painting" />
-                    <img src="images/posterHumanZoo.png" alt="" />
-                    <img src="images/policia-criancasnegras2.png" alt="" />
-                    
-                    <img src="images/SenhoraEscravos1860.png" alt="" />
+    const nextSlide =() =>{
+        setCurrent(current === length -1 ? 0 : current +1)
+    }
+
+    if(!Array.isArray(carousels) || carousels.length <=2){
+        return null;
+    }
+
+    return (
+        <>
+            <IconContext.Provider value={{color: '#e3be92'}} >
+
+                <div className="wrapper">
+                    <ul className="angle-left">
+                    {/* onClick={prevSlide} */}
+                        <AiIcons.AiOutlineDoubleLeft/> 
+                    </ul>
+            
+                    <div className="carouselus">
+
+                        {CarouselData.map((carousel, index) => {
+                            return <img src={carousel.image}  alt='' className='img'/>
+
+                        
+                        })}
+                        {/* <img src="images/01JeanBaptisteDebret1823.png" alt="Debret Painting" />
+                        <img src="images/posterHumanZoo.png" alt="" />
+                        <img src="images/policia-criancasnegras2.png" alt="" />
+                        
+                        <img src="images/SenhoraEscravos1860.png" alt="" /> */}
+                    </div>
+                    <ul className="angle-right" onClick={nextSlide}>
+                        <AiIcons.AiOutlineDoubleRight/>
+                    </ul>
                 </div>
-                <ul className="angle-right">
-                    <AiIcons.AiOutlineDoubleRight/>
-                </ul>
-            </div>
-     
-        </IconContext.Provider>
         
-    
-      
-    
+            </IconContext.Provider>
+            
+        
+        
+        
 
-    </>
-  )
+        </>
+    )
 }
 
 export default Carousel
+
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../../components/Header'
 import Featured from '../../components/Featured'
 import About from '../../components/About'
+import Carousel from '../../components/Carousel';
 import './index.css'
 
 
@@ -20,6 +21,7 @@ const Home = () => {
         <Featured/>
           
         <About/>
+        <Carousel/>
       </div>  
      
     </>
